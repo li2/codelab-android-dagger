24580f8a311b6d70531be4e9e326adf384e4b506
Weiyi Li <weiyi.just2@gmail.com>
2023-10-08 22:03:31 +1300

    Registration - created dagger module RegistrationActivityModule

--- app/src/main/java/com/example/android/dagger/di/ActivityModule.kt
+++ app/src/main/java/com/example/android/dagger/di/ActivityModule.kt
@@ -5,17 +5,12 @@ import com.example.android.dagger.main.MainActivity
 import com.example.android.dagger.settings.SettingsActivity
 import com.example.android.dagger.user.LoggedUserScope
 import com.example.core.common.di.ActivityScope
-import com.example.feature.registration.RegistrationActivity
-import com.example.feature.registration.di.RegistrationFragmentModule
+import com.example.feature.registration.di.RegistrationActivityModule
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
 
-@Module
+@Module(includes = [RegistrationActivityModule::class])
 abstract class ActivityModule {
-    @ActivityScope
-    @ContributesAndroidInjector(modules = [RegistrationFragmentModule::class])
-    abstract fun registrationActivity(): RegistrationActivity
-
     @ActivityScope
     @ContributesAndroidInjector
     abstract fun loginActivity(): LoginActivity
